506c7402261a2099d31c84d15e691bb68a084e33
hexomethyl <86240431+hexomethyl@users.noreply.github.com>
2023-03-18 14:14:14 +1100

    Remove hardcoded font

--- src/carbon.cpp
+++ src/carbon.cpp
@@ -8,7 +8,6 @@ void carbon::initialize() {
 	SHGetFolderPathA(nullptr, CSIDL_FONTS, nullptr, 0, csidl_fonts);
 
 	carbon::segoe_ui = carbon::dx11->register_font(std::string(csidl_fonts) + '\\' + "seguiemj.ttf", 10, FW_THIN, true);
-	carbon::fa_regular = carbon::dx11->register_font("C:\\Users\\Alex\\Documents\\GitHub\\ooodummy\\carbon\\assets\\fa-regular-400.ttf", 30, FW_THIN, true);
 
 	init_default_theme();
 }
